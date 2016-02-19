839d1311f43efd7dbb03d6e104ec60fb97354308
Anthony <aturbanek@wisc.edu>
2016-02-18 18:08:44 -0600

    made new activities

---
+++ app/src/main/java/com/example/anthony/quizv1/results.java
@@ -0,0 +1,13 @@
+package com.example.anthony.quizv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}

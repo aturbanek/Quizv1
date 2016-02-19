c5f38b8cff44b0474e8f86759e137cfd3f2a651b
Anthony <aturbanek@wisc.edu>
2016-02-19 12:03:33 -0600

    text frag almost working

--- app/src/main/java/com/example/anthony/quizv1/TextFrag.java
+++ app/src/main/java/com/example/anthony/quizv1/TextFrag.java
@@ -1,9 +1,12 @@
 package com.example.anthony.quizv1;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.DialogPreference;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +37,8 @@ public class TextFrag extends Fragment {
     private String q3;
     private String q4;
 
+    int score = 0;
+
     private TextView question;
     private RadioButton button1;
     private RadioButton button2;
@@ -44,7 +49,7 @@ public class TextFrag extends Fragment {
     private String [] question2 = {"What days of the week do we have class?", "M W TH",
             "W TH F", "M W F"};
     private String [] question3 = {"What union is the computer science building next to", "Union " +
-            "South", "Memorial Union"};
+            "South", "Memorial Union", "none of the above"};
 
 
     private OnFragmentInteractionListener mListener;
@@ -101,7 +106,139 @@ public class TextFrag extends Fragment {
             button2.setText(question1[2]);
             button3.setText(question1[3]);
         }
+        else if(q2 == null)
+        {
+            question.setText(question2[0]);
+            button1.setText(question2[1]);
+            button2.setText(question2[2]);
+            button3.setText(question2[3]);
+        }
+        else
+        {
+            question.setText(question3[0]);
+            button1.setText(question3[1]);
+            button2.setText(question3[2]);
+            button3.setText(question3[3]);
+        }
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (q1 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container, TextFrag.newInstance(question1[1], null, null, null))
+                            .addToBackStack(null)
+                            .commit();
+
+                } else if (q2 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container, TextFrag.newInstance(q1, question1[2], null, null))
+                            .addToBackStack(null)
+                            .commit();
+                } else {
+                    q3 = question1[3];
+                    score();
+
+                }
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (q1 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container, TextFrag.newInstance(question2[1], null, null, null))
+                            .addToBackStack(null)
+                            .commit();
+
+                } else if (q2 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container, TextFrag.newInstance(q1, question2[2], null, null))
+                            .addToBackStack(null)
+                            .commit();
+                } else {
+                    q3 = question2[3];
+                    score();
+
+                }
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (q1 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container,TextFrag.newInstance(question3[1], null, null, null))
+                            .addToBackStack(null)
+                            .commit();
+
+                } else if (q2 == null) {
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_fragment_container,TextFrag.newInstance(q1,question3[2] , null, null))
+                            .addToBackStack(null)
+                            .commit();
+                } else {
+                    q3 = question3[3];
+                    score();
+
+                }
+            }
+        });
+
+    }
+
+    public void score()
+    {
+        if(q1.equals("407")){
+            score++;
+        }
+        if(q2.equals("M W F"))
+        {
+            score++;
+        }
+        if(q3.equals("Union South"))
+        {
+            score++;
+        }
+
+    }
+
+
+    private void displayWinner(String score) {
+        new AlertDialog.Builder(getActivity())
+                .setCancelable(true)
+                .setTitle("Score")
+                .setMessage(score)
+                .setPositiveButton("Play Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getFragmentManager().popBackStack();
+                        getFragmentManager().popBackStack();
+                        getFragmentManager().popBackStack();
+                    }
+                })
+
+                .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getActivity().finish();
+                    }
+
+                })
+                .show();
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
